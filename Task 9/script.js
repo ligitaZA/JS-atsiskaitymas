da329991e5ae51382650f05f0a47d83ae242b214
ligitaZA <ligitazaicevaite@gmail.com>
2023-01-02 11:28:46 +0200

    task8 completed

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,21 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget){
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive(){
+    return this.budget > 100000000;
+  }
+}
+
+const movie = new Movie ('Harry Potter and the Half-Blood Prince', 'David Yates', 250000000);
+
+console.log(movie.title);
+console.log(movie.director);
+console.log(movie.budget);
+console.log(movie.wasExpensive());
