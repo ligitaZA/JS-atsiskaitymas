a7b59780ed597d2f634f2e1b0656b07da39d55ff
ligitaZA <ligitazaicevaite@gmail.com>
2023-01-02 10:44:48 +0200

    task4 completed

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,22 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+fetch(ENDPOINT)
+  .then(response => response.json())
+  .then(cars => {
+    let html = "";
+    cars.forEach(car => {
+      html += `
+        <div class="car">
+          <h2>${car.brand}</h2>
+          <ul>
+      `;
+      car.models.forEach(model => {
+        html += `<li>${model}</li>`;
+      });
+      html += `</ul></div>`;
+    });
+    // Display the HTML in the output element
+    document.getElementById("output").innerHTML = html;
+  });
