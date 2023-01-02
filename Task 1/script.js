0228be34da628445ce728e465b9717dcb3a03adc
ligitaZA <ligitazaicevaite@gmail.com>
2023-01-02 09:51:32 +0200

    task completed

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,25 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+let form = document.getElementById("form");
+
+document.getElementById("submit-btn").addEventListener("click", function(e) {
+  e.preventDefault();
+  
+  let weightInKg = document.getElementById("search").value;
+  let weightInLbs = weightInKg * 2.2046;
+  let weightInGrams = weightInKg / 0.0010000;
+  let weightInOz = weightInKg * 35.274;
+
+  document.getElementById("output").innerHTML = 
+  `
+  <p><b>${weightInKg} kg converted: </b></p>
+  <p>${weightInLbs} lbs</p>
+  <hr>
+  <p>${weightInGrams} g</p>
+  <hr>
+  <p>${weightInOz} oz</p>
+  `;
+
+  form.reset();
+});
