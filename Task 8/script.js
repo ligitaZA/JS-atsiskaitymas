08abdc09fa20a3f3b02803d854a840747ac589b9
ligitaZA <ligitazaicevaite@gmail.com>
2023-01-02 11:19:53 +0200

    task8 completed

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,26 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+  constructor() {}
+  sum(a,b) {
+    return a + b;
+  }
+  subtraction(a, b){
+    return a - b;
+  }
+  multiplication(a,b){
+    return a * b;
+  }
+  division(a, b){
+    return a / b;
+  }
+}
+
+const calculate = new Calculator();
+
+console.log(calculate.sum(4, 27)); 
+console.log(calculate.subtraction(64, 23));
+console.log(calculate.multiplication(22, 12)); 
+console.log(calculate.division(500, 45)); 
